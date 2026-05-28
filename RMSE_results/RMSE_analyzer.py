00b41b28241500e950e159cd4971ffbc47063bfe
matte <matteoricci145@gmail.com>
2026-05-28 21:22:29 +0200

    Added some plots

---
+++ RMSE_results/RMSE_analyzer.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+# Cylinders dimension
+D = [10, 12.8, 15.9, 19, 22.3, 25.6]
+nu = [8, 12, 17]
+
+for j in nu:
+    data_frames = []
+    for i in D:
+        df = pd.read_csv(f"./{i}/risultati_{j}_{i}.csv")
+        df["cylinder_dimension"] = i # Adding a coloum with D value in the starting csv
+        data_frames.append(df)
+
+    df_all = pd.concat(data_frames, ignore_index=True)
+
+    df_all['distance_in_lamda'] = df_all['distance_in_lamda'].round(2)
+    df_pivot = df_all.pivot(index="distance_in_lamda", columns="cylinder_dimension", values="RMSE")
+    
+    df_pivot = df_pivot.replace(np.inf, np.nan)
+    
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    sns.heatmap(df_pivot, annot=True, cmap="YlGnBu")
+    plt.title(f"RMSE with {j}-EDGES cylinder")
+    plt.xlabel("Cylinder dimension [lambda]")
+    plt.ylabel("Distance from cylinder [m]")
+    plt.savefig(f"RMSE_{j}")
+    plt.close()
+    
+
